6e92ec34bfad49fd97e48173d24aea4927409883
MrFr3di <ynagi-wm@mail.ru>
2026-07-20 02:29:06 +0500

    refactor: expose only database initialization capability

--- core/database/src/main/kotlin/app/muxtv/database/MuxTvDatabaseFactory.kt
+++ core/database/src/main/kotlin/app/muxtv/database/MuxTvDatabaseFactory.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import androidx.room3.Room
 
 object MuxTvDatabaseFactory {
-    fun create(context: Context): MuxTvDatabase = Room.databaseBuilder(
-        context = context.applicationContext,
-        klass = MuxTvDatabase::class.java,
-        name = "muxtv.db",
-    ).build()
+    fun createInitializer(context: Context): DatabaseInitializer {
+        val database = Room.databaseBuilder(
+            context = context.applicationContext,
+            klass = MuxTvDatabase::class.java,
+            name = "muxtv.db",
+        ).build()
+        return DatabaseInitializer(database)
+    }
 }
