58e64e93bd1f21c1a59d143118d4c1ab45d61a37
MrFr3di <ynagi-wm@mail.ru>
2026-07-20 01:39:05 +0500

    build: configure reproducible Room3 schemas

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("muxtv.android.library")
     alias(libs.plugins.ksp)
+    alias(libs.plugins.room3)
 }
 
 android {
@@ -8,16 +9,21 @@ android {
     defaultConfig { testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner" }
 }
 
+room3 {
+    schemaDirectory("$projectDir/schemas")
+}
+
 dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:model"))
+    implementation(libs.coroutines.android)
     implementation(libs.room3.runtime)
     ksp(libs.room3.compiler)
     testImplementation(libs.junit)
     testImplementation(libs.truth)
     androidTestImplementation(libs.androidx.test.runner)
     androidTestImplementation(libs.androidx.test.core)
+    androidTestImplementation(libs.coroutines.test)
+    androidTestImplementation(libs.room3.testing)
     androidTestImplementation(libs.truth)
 }
-
-ksp { arg("room.schemaLocation", "$projectDir/schemas") }
