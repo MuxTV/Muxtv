2a872e5a109b8c8444a66002495940cf97c058ca
MrFr3di <ynagi-wm@mail.ru>
2026-07-21 22:22:02 +0500

    feat: expose shared database components

--- core/database/src/main/kotlin/app/muxtv/database/MuxTvDatabaseFactory.kt
+++ core/database/src/main/kotlin/app/muxtv/database/MuxTvDatabaseFactory.kt
@@ -3,13 +3,24 @@ package app.muxtv.database
 import android.content.Context
 import androidx.room3.Room
 
+class MuxTvDatabaseComponents internal constructor(
+    val initializer: DatabaseInitializer,
+    val sourceRevisionStore: SourceRevisionStore,
+)
+
 object MuxTvDatabaseFactory {
-    fun createInitializer(context: Context): DatabaseInitializer {
+    fun create(context: Context): MuxTvDatabaseComponents {
         val database = Room.databaseBuilder(
             context = context.applicationContext,
             klass = MuxTvDatabase::class.java,
             name = "muxtv.db",
         ).build()
-        return DatabaseInitializer(database)
+        return MuxTvDatabaseComponents(
+            initializer = DatabaseInitializer(database),
+            sourceRevisionStore = RoomSourceRevisionStore(database.sourceRevisionDao()),
+        )
     }
+
+    fun createInitializer(context: Context): DatabaseInitializer =
+        create(context).initializer
 }
