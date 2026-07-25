a4662b986607b279d2a2e083678209542a082c82
MrFr3di <ynagi-wm@mail.ru>
2026-07-25 17:44:27 +0500

    fix: import secure locator length filter

--- feature/sources/src/main/kotlin/app/muxtv/feature/sources/AddSourceRoute.kt
+++ feature/sources/src/main/kotlin/app/muxtv/feature/sources/AddSourceRoute.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.text.input.InputTransformation
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.input.TextObfuscationMode
 import androidx.compose.foundation.text.input.clearText
+import androidx.compose.foundation.text.input.maxLength
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
