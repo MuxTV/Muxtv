700d627bca0ba9a5493230f78447b168449b74b4
MrFr3di <ynagi-wm@mail.ru>
2026-07-28 00:13:42 +0500

    test: add deterministic corpus contract dependencies

--- core/testing/build.gradle.kts
+++ core/testing/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins { id("muxtv.kotlin.library") }
 
 dependencies {
+    testImplementation(project(":catalog:ingest"))
+    testImplementation(libs.coroutines.test)
     testImplementation(libs.junit)
     testImplementation(libs.truth)
 }
