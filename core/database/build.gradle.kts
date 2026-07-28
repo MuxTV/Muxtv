e1ff35c48b7c906e790a6f530e4ea1a039878120
MrFr3di <ynagi-wm@mail.ru>
2026-07-28 23:34:13 +0500

    test: isolate catalog database measurements

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,12 +1,31 @@
+import org.gradle.api.GradleException
+
 plugins {
     id("muxtv.android.library")
     alias(libs.plugins.ksp)
     alias(libs.plugins.room3)
 }
 
+val catalogMeasurementsEnabled = providers.gradleProperty("catalogMeasurements")
+    .orElse("false")
+    .map { rawValue ->
+        when (rawValue.lowercase()) {
+            "true" -> true
+            "false" -> false
+            else -> throw GradleException("catalogMeasurements must be true or false.")
+        }
+    }
+
 android {
     namespace = "app.muxtv.database"
-    defaultConfig { testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner" }
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testApplicationId = "app.muxtv.database.test"
+        if (!catalogMeasurementsEnabled.get()) {
+            testInstrumentationRunnerArguments["notAnnotation"] =
+                "app.muxtv.database.CatalogDatabaseMeasurement"
+        }
+    }
 }
 
 room3 {
