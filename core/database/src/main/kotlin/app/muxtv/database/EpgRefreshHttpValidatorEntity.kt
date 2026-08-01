746d1b39e7c353440ae7c5b400cb99bb8efacb44
MrFr3di <ynagi-wm@mail.ru>
2026-08-01 17:27:45 +0500

    feat: isolate EPG HTTP validator persistence

---
+++ core/database/src/main/kotlin/app/muxtv/database/EpgRefreshHttpValidatorEntity.kt
@@ -0,0 +1,34 @@
+package app.muxtv.database
+
+import androidx.room3.Entity
+import androidx.room3.ForeignKey
+
+@Entity(
+    tableName = "epg_refresh_http_validators",
+    foreignKeys = [
+        ForeignKey(
+            entity = EpgSourceEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["sourceId"],
+            onDelete = ForeignKey.CASCADE,
+        ),
+    ],
+    primaryKeys = ["sourceId"],
+)
+data class EpgRefreshHttpValidatorEntity(
+    val sourceId: String,
+    val etag: String? = null,
+    val lastModified: String? = null,
+    val updatedAtEpochMillis: Long,
+) {
+    init {
+        require(sourceId.isNotBlank())
+        require(etag == null || etag.isNotBlank())
+        require(lastModified == null || lastModified.isNotBlank())
+        require(updatedAtEpochMillis >= 0)
+    }
+
+    override fun toString(): String =
+        "EpgRefreshHttpValidatorEntity(sourceId=<redacted>, etagPresent=${etag != null}, " +
+            "lastModifiedPresent=${lastModified != null}, updatedAtEpochMillis=$updatedAtEpochMillis)"
+}
