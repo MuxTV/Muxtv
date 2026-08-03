f497aae19f2626db844013753c8032d1d2cc26f0
MrFr3di <ynagi-wm@mail.ru>
2026-08-03 11:21:02 +0500

    feat: wire channel preferences into app journey

--- app/tv/src/androidTest/kotlin/app/muxtv/AppNavigationSourceJourneyTest.kt
+++ app/tv/src/androidTest/kotlin/app/muxtv/AppNavigationSourceJourneyTest.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsFocused
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodes
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
@@ -70,6 +71,7 @@ class AppNavigationSourceJourneyTest {
                 MuxTvTheme {
                     AppNavigation(
                         playbackCatalog = playbackCatalog,
+                        channelPreferencesRepository = NoChannelPreferencesRepository,
                         epgGuideRepository = NoGuideEpgGuideRepository,
                         controllerConnector = controllerConnector,
                         sourceRefreshStore = sourceStore,
