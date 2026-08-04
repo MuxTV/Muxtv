c44920b2e2fca9730a0b2896943c082070e5e023
MrFr3di <ynagi-wm@mail.ru>
2026-08-04 12:24:51 +0500

    test: forbid alternative PlaybackEngine DI owner

---
+++ app/tv/src/test/kotlin/app/muxtv/di/PlaybackOwnershipBindingTest.kt
@@ -0,0 +1,18 @@
+package app.muxtv.di
+
+import app.muxtv.player.PlaybackEngine
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class PlaybackOwnershipBindingTest {
+    @Test
+    fun `application DI does not provide an alternative PlaybackEngine owner`() {
+        val playbackEngineProviders = AppModule::class.java.declaredMethods
+            .filter { method -> PlaybackEngine::class.java.isAssignableFrom(method.returnType) }
+            .map { method -> method.name }
+
+        assertThat(playbackEngineProviders)
+            .named("PlaybackEngine providers outside MuxTvPlaybackService ownership")
+            .isEmpty()
+    }
+}
