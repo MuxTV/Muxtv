6086241f72994a6bd1fdd688fa3024a06e51b2f7
MrFr3di <ynagi-wm@mail.ru>
2026-08-06 00:45:31 +0500

    test(database): use Guide viewport invalidation DAO

--- core/database/src/androidTest/kotlin/app/muxtv/database/GuideProgrammeWindowRepositoryTest.kt
+++ core/database/src/androidTest/kotlin/app/muxtv/database/GuideProgrammeWindowRepositoryTest.kt
@@ -25,10 +25,9 @@ class GuideProgrammeWindowRepositoryTest {
         ).build()
         DatabaseInitializer(database).initialize()
         val sourceStore = RoomSourceRevisionStore(database.sourceRevisionDao())
-        val epgGuideRepository = RoomEpgGuideRepository(database.epgGuideDao())
         repository = RoomGuideWindowRepository(
             dao = database.guideWindowDao(),
-            invalidationSource = epgGuideRepository,
+            invalidationDao = database.guideWindowInvalidationDao(),
         )
 
         sourceStore.upsertSource(SourceDefinition(SOURCE_ID, "Programme source"))
