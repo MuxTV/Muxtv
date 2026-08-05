51468dfbf741eec52176e46e27166db37c7a23b6
MrFr3di <ynagi-wm@mail.ru>
2026-08-06 00:39:30 +0500

    fix(build): use Gradle work cache annotation

--- build-logic/convention/src/main/kotlin/app/muxtv/buildlogic/VerifyCurrentRoomSchemaTask.kt
+++ build-logic/convention/src/main/kotlin/app/muxtv/buildlogic/VerifyCurrentRoomSchemaTask.kt
@@ -6,7 +6,6 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.tasks.DisableCachingByDefault
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
@@ -14,6 +13,7 @@ import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecOperations
+import org.gradle.work.DisableCachingByDefault
 
 @DisableCachingByDefault(
     because = "The task verifies repository working-tree state and has no reusable output.",
