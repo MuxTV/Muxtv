6605f2e6c65649f2f4e02ae4f27b3a58086ebfc4
MrFr3di <ynagi-wm@mail.ru>
2026-08-06 17:09:57 +0500

    test: use Compose Test v2 for new shared focus journey (#111)

--- app/tv/src/androidTest/kotlin/app/muxtv/MuxTvFocusSurfaceInteractionTest.kt
+++ app/tv/src/androidTest/kotlin/app/muxtv/MuxTvFocusSurfaceInteractionTest.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.assertIsFocused
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performKeyInput
 import androidx.compose.ui.unit.dp
