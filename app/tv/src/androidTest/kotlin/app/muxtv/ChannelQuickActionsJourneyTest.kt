285e7609272ef0999201d3d3b09442de26764bb1
MrFr3di <ynagi-wm@mail.ru>
2026-08-31 21:56:06 +0500

    test(channels): fix compose test assertion imports

--- app/tv/src/androidTest/kotlin/app/muxtv/ChannelQuickActionsJourneyTest.kt
+++ app/tv/src/androidTest/kotlin/app/muxtv/ChannelQuickActionsJourneyTest.kt
@@ -2,8 +2,6 @@ package app.muxtv
 
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.semantics.SemanticsActions
-import androidx.compose.ui.test.assertDoesNotExist
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.assertIsFocused
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
