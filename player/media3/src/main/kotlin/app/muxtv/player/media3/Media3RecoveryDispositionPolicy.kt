2c26428f3f74e805e2cccd772e75a7bd8b3f4988
MrFr3di <ynagi-wm@mail.ru>
2026-09-13 07:55:26 +0500

    feat(c11): add pure Media3 recovery disposition policy

---
+++ player/media3/src/main/kotlin/app/muxtv/player/media3/Media3RecoveryDispositionPolicy.kt
@@ -0,0 +1,37 @@
+package app.muxtv.player.media3
+
+import androidx.annotation.OptIn as AndroidXOptIn
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.util.UnstableApi
+import app.muxtv.player.PlaybackFailureCategory
+
+internal enum class Media3RecoveryPolicyVariant {
+    A_CURRENT_GENERIC_NEXT,
+    B_NARROW_TYPED_STOP,
+    C_BROAD_RENDER_STOP,
+}
+
+@AndroidXOptIn(UnstableApi::class)
+internal object Media3RecoveryDispositionPolicy {
+    fun disposition(
+        variant: Media3RecoveryPolicyVariant,
+        failure: Media3Failure,
+    ): PlaybackRecoveryDisposition = when (variant) {
+        Media3RecoveryPolicyVariant.A_CURRENT_GENERIC_NEXT ->
+            PlaybackRecoveryDisposition.TRY_NEXT_CANDIDATE
+
+        Media3RecoveryPolicyVariant.B_NARROW_TYPED_STOP ->
+            if (failure.media3ErrorCode == PlaybackException.ERROR_CODE_FAILED_RUNTIME_CHECK) {
+                PlaybackRecoveryDisposition.STOP_RECOVERY
+            } else {
+                PlaybackRecoveryDisposition.TRY_NEXT_CANDIDATE
+            }
+
+        Media3RecoveryPolicyVariant.C_BROAD_RENDER_STOP ->
+            if (failure.category == PlaybackFailureCategory.PLAYER_RENDER) {
+                PlaybackRecoveryDisposition.STOP_RECOVERY
+            } else {
+                PlaybackRecoveryDisposition.TRY_NEXT_CANDIDATE
+            }
+    }
+}
